db9478d02aaa57fc250ce5bffa81e265e5322132
Ivan-Gusev-1993 <ivangusev1993@gmail.com>
2025-03-14 21:24:08 +0300

    last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React, {useState} from 'react';
-import styles from "./components/Site.module.css";
+import React from 'react';
 import {PageOne} from "./components/pages/PageOne";
 import {PageTwo} from "./components/pages/PageTwo";
 import {PageThree} from "./components/pages/PageThree";
@@ -7,33 +6,37 @@ import {Navigate, NavLink, Route, Routes} from 'react-router-dom';
 import {Error404} from "./components/pages/Error404";
 import {S} from './components/pages/_styles'
 
+const PATH = {
+    PAGE1: "/page1",
+    PAGE2: "/page2",
+    PAGE3: "/page3",
+    ERROR: "/page/error",
+} as const;
 
 function App() {
     return (
         <div>
-            <div className={styles.header}><h1>HEADER</h1></div>
-            <div className={styles.body}>
-                <div className={styles.nav}>
-                    <S.NavWrapper><NavLink to={"/page1"}>page1</NavLink></S.NavWrapper>
-                    <S.NavWrapper><NavLink to={"/page2"}>page2</NavLink></S.NavWrapper>
-                    <S.NavWrapper><NavLink to={"/page3"}>page3</NavLink></S.NavWrapper>
-                </div>
-                <div className={styles.content}>
-
-
+            <S.Header><h1>HEADER</h1></S.Header>
+            <S.Body>
+                <S.Nav>
+                    <S.NavWrapper><NavLink to={PATH.PAGE1}>page1</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.PAGE2}>page2</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.PAGE3}>page3</NavLink></S.NavWrapper>
+                </S.Nav>
+                <S.Content>
                     <Routes>
                         <Route path="/" element={<Navigate to={"/page1"}/>}/>
 
-                        <Route path="/page1" element={<PageOne/>}/>
-                        <Route path="/page2" element={<PageTwo/>}/>
-                        <Route path="/page3" element={<PageThree/>}/>
-                        <Route path="/page/error" element={<Error404/>}/>
-                        <Route path="*" element={<Navigate to={"/page/error"}/>}/>
+                        <Route path={PATH.PAGE1} element={<PageOne/>}/>
+                        <Route path={PATH.PAGE2} element={<PageTwo/>}/>
+                        <Route path={PATH.PAGE3} element={<PageThree/>}/>
+                        <Route path={PATH.ERROR} element={<Error404/>}/>
+                        <Route path="*" element={<Navigate to={PATH.ERROR}/>}/>
                     </Routes>
 
-                </div>
-            </div>
-            <div className={styles.footer}>abibas 2023</div>
+                </S.Content>
+            </S.Body>
+            <S.Footer>abibas 2023</S.Footer>
         </div>
     );
 }
