9d637432f90f9ab6d9b40260bd04766069791c86
Ivan Gusev <ivangusev1993@gmail.com>
2025-04-11 22:23:37 +0300

    renamed components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {PageOne} from "./components/pages/PageOne";
-import {PageTwo} from "./components/pages/PageTwo";
-import {PageThree} from "./components/pages/PageThree";
+import {Adidas} from "./components/pages/Adidas";
+import {Puma} from "./components/pages/Puma";
+import {Fila} from "./components/pages/Fila";
 import {Navigate, NavLink, Route, Routes} from 'react-router-dom';
 import {Error404} from "./components/pages/Error404";
 import {S} from './components/pages/_styles'
@@ -20,18 +20,22 @@ function App() {
             <S.Header><h1>HEADER</h1></S.Header>
             <S.Body>
                 <S.Nav>
-                    <S.NavWrapper><NavLink to={PATH.PAGE1}>page1</NavLink></S.NavWrapper>
-                    <S.NavWrapper><NavLink to={PATH.PAGE2}>page2</NavLink></S.NavWrapper>
-                    <S.NavWrapper><NavLink to={PATH.PAGE3}>page3</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.PAGE1}>Adidas</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.PAGE2}>Puma</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.PAGE3}>Fila</NavLink></S.NavWrapper>
                 </S.Nav>
                 <S.Content>
                     <Routes>
                         <Route path="/" element={<Navigate to={"/page1"}/>}/>
 
-                        <Route path={PATH.PAGE1} element={<PageOne/>}/>
-                        <Route path={PATH.PAGE2} element={<PageTwo/>}/>
-                        <Route path={PATH.PAGE3} element={<PageThree/>}/>
-                        <Route path="*" element={<Navigate to={PATH.ERROR} replace />} />
+                        <Route path={PATH.PAGE1} element={<Adidas/>}/>
+                        <Route path={PATH.PAGE2} element={<Puma/>}/>
+                        <Route path={PATH.PAGE3} element={<Fila/>}/>
+
+                        <Route path={'/*'} element={<Error404/>}/>
+
+                        {/*<Route path={'/page/error'} element={<Error404/>}/>*/}
+                        {/*<Route path={'/*'} element={<Navigate to={'/page/error'}/>}/>*/}
                     </Routes>
 
                 </S.Content>
